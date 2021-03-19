e873b85e3f67872f07271fc909aed76021e8e748
RYOTA <kobaryo04ysh@icloud.com>
2021-03-20 01:14:58 +0900

    feat: cardをスワイプできるように修正

---
+++ src/Components/TinderCards.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import TinderCard from 'react-tinder-card'
+
+type data = {
+  id: number
+  name: string
+  age: number
+  img: string
+}
+
+type Props = {
+  users: data[]
+}
+
+const TinderCards: React.FC<Props> = (props) => {
+  return (
+    <div className="card-container">
+      {props.users.map((user: data) => (
+        <div className="person-photo" key={user.id}>
+          <TinderCard preventSwipe={['up', 'down']}>
+            <div className="person-photo-inner">
+              <img src={user.img} alt="" />
+              <div className="person-info">
+                <strong>{user.name}</strong>, {user.age}
+              </div>
+            </div>
+          </TinderCard>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default TinderCards
