81dbfdc1ad864e8c63a4b14bf6ee3848fc54cc0e
RYOTA <kobaryo04ysh@icloud.com>
2021-03-19 19:44:33 +0900

    test: add test

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,9 +1,6 @@
-import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import App from '../Components/App'
 
-test('renders learn react link', () => {
+test('Appコンポーネントが正常に描画される', () => {
   render(<App />)
-  const linkElement = screen.getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
 })
