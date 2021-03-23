af9d38fc7585817f655fab756b4821e27a02939c
RYOTA <kobaryo04ysh@icloud.com>
2021-03-23 21:30:27 +0900

    test: TinderCardsのレンダリングのテストを追加

---
+++ src/__tests__/TinderCards.test.tsx
@@ -0,0 +1,24 @@
+import { render } from '@testing-library/react'
+import TinderCards from '../Components/TinderCards'
+import setCurrentId from '../Components/TinderCards'
+
+const TinderCardsComponent = (
+  <TinderCards users={[]} currentId={1} setCurrentId={() => setCurrentId} />
+)
+
+describe('TinderCardsのレンダリングテスト', () => {
+  it('TinderCardsコンポーネントがレンダリングされているか', () => {
+    render(TinderCardsComponent)
+  })
+  it('ユーザーの情報を表示するcard-containerが存在する', () => {
+    const { container } = render(
+      <TinderCards
+        users={[]}
+        currentId={1}
+        setCurrentId={() => setCurrentId}
+      />,
+    )
+
+    expect(container.getElementsByClassName('card-container').length).toEqual(1)
+  })
+})
