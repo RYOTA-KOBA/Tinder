9c06faa07dac5404fe1cad5707473872be0b47ea
RYOTA <kobaryo04ysh@icloud.com>
2021-03-24 13:12:51 +0900

    feat: cardの下部のユーザー名と年れの部分を押すと開くmodalを実装

--- src/Components/TinderCards.tsx
+++ src/Components/TinderCards.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import TinderCard from 'react-tinder-card'
+import { AiFillCloseCircle } from 'react-icons/ai'
 
 type data = {
   id: number
@@ -15,20 +16,40 @@ type Props = {
 }
 
 const TinderCards: React.FC<Props> = (props) => {
+  const [isOpen, setIsOpen] = useState<Boolean>(false)
+
+  const handleClickOpen = () => {
+    setIsOpen(true)
+  }
+  const handleClickClose = () => {
+    setIsOpen(false)
+  }
+
   return (
     <div className="card-container">
       {props.users.map((user: data) => (
         <div className="person-photo" id={`user-${user.id}`} key={user.id}>
           <TinderCard
             preventSwipe={['up', 'down']}
-            onSwipe={() =>
+            onSwipe={() => {
               props.setCurrentId((currentId: number) => currentId - 1)
-            }
+              // setIsOpen(false)
+            }}
           >
             <div className="person-photo-inner">
               <img src={user.img} alt="" />
-              <div className="person-info">
+              <div className="person-info" onClick={handleClickOpen}>
                 <strong>{user.name}</strong>, {user.age}
+              </div>
+            </div>
+            <div className={isOpen ? 'modal-info' : 'modal-info-hide'}>
+              <div className="modal-info-inner">
+                <AiFillCloseCircle
+                  className="closeicon"
+                  onClick={handleClickClose}
+                />
+                <p>Name: {user.name}</p>
+                <p>Age: {user.age}</p>
               </div>
             </div>
           </TinderCard>
